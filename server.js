5dba05ace21738f30bcf89f9040685908c004a89
ruslan-fatkullov <fatkullov1999@gmail.com>
2023-06-22 14:34:02 +0400

    set port

--- server.js
+++ server.js
@@ -29,12 +29,28 @@ app.get("/", function(request, response){
   response.sendFile(__dirname + "/index.html");
 });
 
+const mysql = require("mysql2");
+  
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "testdb",
+  password: ""
+});
+ connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, '80.78.245.52',() => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-const db = require("./app/models");
+/*const db = require("./app/models");
 db.sequelize.sync();
-
+*/
