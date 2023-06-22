091bc671ceb27d8b19cd9fc5e19639c2ffb8bd97
ruslan-fatkullov <fatkullov1999@gmail.com>
2023-06-22 13:59:28 +0400

    set port

--- app/models/index.js
+++ app/models/index.js
@@ -4,7 +4,6 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-  port: dbConfig.PORT,
   operatorsAliases: false,
 
   pool: {
