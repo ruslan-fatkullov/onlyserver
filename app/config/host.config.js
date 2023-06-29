7bbf22ccaaadb271302ea8c65a43e73cdf101425
Фаткуллов Руслан Альфредович <fatkullov_ra@esvoavia.ru>
2023-06-29 10:56:30 +0400

    change main windows

--- app/config/host.config.js
+++ app/config/host.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  MAIL_DOMAIN: 'no-reply@get-esvo-launcher.ru',
-
+  HOST: "http://localhost:8080",
+  CLIENT_HOST: "http://localhost:8081",
+  MAIL_DOMAIN: "no-reply@get-esvo-launcher.ru",
+  mail: {
+    auth: 'fatkullov@inbox.ru',
+    pass: '0T5hwbAcxS3Pxzma8Fpj'
+  }
 };
