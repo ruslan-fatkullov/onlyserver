6ba4df8db3ff83fc7668d00a6fd9ca444218a57b
Фаткуллов Руслан Альфредович <fatkullov_ra@esvoavia.ru>
2023-06-29 13:03:27 +0400

    change main windows

--- app/controller/launcher.Controller.js
+++ app/controller/launcher.Controller.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const file_config = require("../file_directory/file.config")
 
 exports.DownLoadLauncher = (req, res) => {
-    if(fs.existsSync(file_config.FILE_DIRECTORY+"\\setup.exe")){
-        return res.download(file_config.FILE_DIRECTORY + "\\setup.exe")
+    if(fs.existsSync(file_config.FILE_DIRECTORY+"\setup.exe")){
+        return res.download(file_config.FILE_DIRECTORY + "\setup.exe")
         
     }
     return res.status(400).json({message: "К сожалению не получается скачать файл"})
