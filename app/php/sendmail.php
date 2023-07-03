f97ca0e85b3da1ddbbf5cc68ad0596c462a4234e
Фаткуллов Руслан Альфредович <fatkullov_ra@esvoavia.ru>
2023-07-03 10:17:16 +0400

    php mail

--- app/php/sendmail.php
+++ app/php/sendmail.php
@@ -1,5 +1,5 @@
 <?php 
-/*$to  = "<fatkullov@inbox.ru>, " ; 
+$to  = "<fatkullov@inbox.ru>, " ; 
 
 $subject = "Заголовок письма"; 
 
@@ -9,6 +9,5 @@ $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
 $headers .= "From: От кого письмо <no-reply@get-esvo-launcher.ru>\r\n"; 
 $headers .= "Reply-To: fatkullov@inbox.ru\r\n"; 
 
-mail($to, $subject, $message, $headers); */
-echo "php скрипт";
+mail($to, $subject, $message, $headers); 
 ?>
