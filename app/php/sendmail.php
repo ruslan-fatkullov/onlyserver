12a4b10b47d07da570b1f96da8111af83aa0aa9f
Фаткуллов Руслан Альфредович <fatkullov_ra@esvoavia.ru>
2023-07-04 11:10:52 +0400

    php mail

--- app/php/sendmail.php
+++ app/php/sendmail.php
@@ -4,7 +4,7 @@
 $headers  = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=utf-8\r\n";
 $headers .= "To: <fatkullov@inbox.ru>\r\n";
-$headers .= "From: <no-reply@get-esvo-launcher.ru>\r\n";
+$headers .= "From: <noreply@www.get-esvo-launcher.ru>\r\n";
 // Сообщение для Email
 $message = '
         <html>
