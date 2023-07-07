edd8452da260da7000d0c65104b7dad0a96229f5
Фаткуллов Руслан Альфредович <fatkullov_ra@esvoavia.ru>
2023-07-07 15:19:40 +0400

    git init

--- app/mail.sender.js
+++ app/mail.sender.js
@@ -6,50 +6,35 @@ const nodemailer = require('nodemailer');
 
 exports.sendMessage = async (email, tokenOrPassword, subject) => {
 
-    // Generate SMTP service account from ethereal.email
-    nodemailer.createTestAccount((err, account) => {
+    // Create a SMTP transporter object
+    let transporter = nodemailer.createTransport({
+        host: 'mail.get-esvo-launcher.ru',
+        port: 587,
+        secure: true,
+        auth: {
+            user: 'fatkullov@inbox.ru',
+            pass: 'YURwtL00AtUJFjhAqk0c'
+        }
+    });
+
+    // Message object
+    let message = {
+        from: 'Sender Name <noreply@get-esvo-launcher.ru>',
+        to: 'Recipient <fatkullov1999@yandex.ru>',
+        subject: 'Nodemailer is unicode friendly ✔',
+        text: 'Hello to myself!',
+        html: '<p><b>Hello</b> to myself!</p>'
+    };
+
+    transporter.sendMail(message, (err, info) => {
         if (err) {
-            console.error('Failed to create a testing account. ' + err.message);
+            console.log('Error occurred. ' + err.message + " \ " + err.name + " \ " + err.stack);
             return process.exit(1);
         }
 
-        console.log('Credentials obtained, sending message...');
-        console.log(account.user);
-        console.log(account.pass);
-        console.log(account.smtp.host);
-        console.log(account.smtp.port);
-        console.log(account.smtp.secure);
-
-        // Create a SMTP transporter object
-        let transporter = nodemailer.createTransport({
-            host: account.smtp.host,
-            port: account.smtp.port,
-            secure: account.smtp.secure,
-            auth: {
-                user: account.user,
-                pass: account.pass
-            }
-        });
-
-        // Message object
-        let message = {
-            from: 'Sender Name <'+account.user+'>',
-            to: 'Recipient <fatkullov@inbox.ru>',
-            subject: 'Nodemailer is unicode friendly ✔',
-            text: 'Hello to myself!',
-            html: '<p><b>Hello</b> to myself!</p>'
-        };
-
-        transporter.sendMail(message, (err, info) => {
-            if (err) {
-                console.log('Error occurred. ' + err.message + " \ " + err.name +  " \ " + err.stack);
-                return process.exit(1);
-            }
-
-            console.log('Message sent: %s', info.messageId);
-            // Preview only available when sending through an Ethereal account
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        });
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     });
 
 }
