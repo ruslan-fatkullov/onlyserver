6f51dff141a2e2fdea76b8aa3ed796add131da6a
Фаткуллов Руслан Альфредович <fatkullov_ra@esvoavia.ru>
2023-07-14 09:24:07 +0400

    git init

--- app/mail.sender.js
+++ app/mail.sender.js
@@ -8,7 +8,7 @@ exports.sendMessage = async (email, tokenOrPassword, subject) => {
     console.log("Отправка письма...")
 
     let transporter = nodemailer.createTransport({
-        host: "localhost",
+        host: "smtp.get-esvo-launcher.ru",
         port: 587,
         secure: false,
         /*auth: {
